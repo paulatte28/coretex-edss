416936bbb3ed72c89ba48b49909766c988622df8
paulatte28 <kcoretico@gmail.com>
2026-04-08 23:43:52 +0800

    dashboard admin

--- obj/Debug/net8.0/coretex-finalproj.AssemblyInfo.cs
+++ obj/Debug/net8.0/coretex-finalproj.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("coretex-finalproj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fa47ca2aebc6e73e933047dbb0af5a720110eee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7c0769326343ec846c02ec8cf02f9147dcbfb18")]
 [assembly: System.Reflection.AssemblyProductAttribute("coretex-finalproj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("coretex-finalproj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
