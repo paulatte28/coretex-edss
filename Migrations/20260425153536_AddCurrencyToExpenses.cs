08c880d658ca7f58c51602fc33f34c692f4468a5
paulatte28 <kcoretico@gmail.com>
2026-04-25 23:49:56 +0800

    fixed exchange rate api

---
+++ Migrations/20260425153536_AddCurrencyToExpenses.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace coretex_finalproj.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCurrencyToExpenses : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Currency",
+                table: "Expenses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Currency",
+                table: "Expenses");
+        }
+    }
+}
